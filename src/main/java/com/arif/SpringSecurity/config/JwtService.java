1a165105c38e41b350de7c40ace145a5029bb692
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2023-01-14 16:08:18 +0300

    JwtAuthenticationFilter, JwtService

---
+++ src/main/java/com/arif/SpringSecurity/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.arif.SpringSecurity.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extreactUsername(String token) {
+        return null;
+    }
+}
---
+++ src/main/java/com/arif/SpringSecurity/user/Role.java
@@ -0,0 +1,8 @@
+package com.arif.SpringSecurity.user;
+
+public enum Role {
+
+    USER,
+
+    ADMIN
+}
