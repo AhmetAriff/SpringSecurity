4125ac53ff13def3b43f97097845549307214b25
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2023-01-15 17:52:41 +0300

    ApplicationConfig()

---
+++ src/main/java/com/arif/SpringSecurity/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.arif.SpringSecurity.config;
+
+import com.arif.SpringSecurity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByMail(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
+
+
+}
